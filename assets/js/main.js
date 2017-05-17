4453923e0572aa51aacbe891329c1717ea7cced0
caroquintana <caro.quintana.n@gmail.com>
2017-05-17 00:23:46 -0400

    2/3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 function mostrarMensaje(){
 	var boton = document.getElementById("demo");
 	boton.addEventListener("click", function(){
-		alert("hola estoy sobre el menu");
+		alert("hola, estoy sobre el menu");
 	});	
 }
 mostrarMensaje();
+
+function paginaCargada(){
+	alert("La página ya está cargada");
+   };
+
+function sobreMi(){
+	alert("¡Estás sobre mí!");
+}
